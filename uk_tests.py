1a88e87a8a4eaa4f143a942ee99f80968db8ea7e
Teodor Ivanov <teodor@magicsolutions.bg>
2017-01-14 14:06:09 +0200

    Fix login tests and add item to cart test

--- uk_tests.py
+++ uk_tests.py
@@ -1,4 +1,4 @@
-import ipdb
+import time
 import unittest
 from selenium import webdriver
 
@@ -25,6 +25,11 @@ class BookStoreTestCase(unittest.TestCase):
         page_source = self.browser.page_source
         self.assertNotIn('Internal Server Error', page_source)
 
+        self.browser.find_element_by_id('Header_Menu_Field1').click()
+        time.sleep(1)
+        page_source = self.browser.page_source
+        self.assertIn('value="Logout"', page_source)
+
     def testLogInAdmin(self):
         self.browser.get(self.login_url)
         username = self.browser.find_element_by_id('Login_name')
@@ -35,10 +40,21 @@ class BookStoreTestCase(unittest.TestCase):
         page_source = self.browser.page_source
         self.assertNotIn('Internal Server Error', page_source)
 
+        self.browser.find_element_by_id('Header_Menu_Field1').click()
+        time.sleep(1)
+        page_source = self.browser.page_source
+        self.assertIn('value="Logout"', page_source)
+
     def testAddItemToCart(self):
         self.testLogInAdmin()
         self.browser.get(self.page_url)
         self.browser.find_element_by_css_selector('a[href^="BookDetail"]:first-of-type').click()
+        time.sleep(1)
+        item_name = self.browser.find_element_by_id('Detail_name').text
+        self.browser.find_element_by_id('Order_insert').click()
+        time.sleep(1)
+        page_source = self.browser.page_source
+        self.assertIn(item_name, page_source)
 
 
 if __name__ == '__main__':
